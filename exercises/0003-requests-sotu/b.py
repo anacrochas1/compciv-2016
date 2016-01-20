166b86b5c1e616685805863a35c075c5a55d5639
anacrochas1 <anasantos@stanford.edu>
2016-01-20 01:28:24 -0800

    homework done

---
+++ exercises/0003-requests-sotu/b.py
@@ -0,0 +1,4 @@
+import requests
+url = "http://www.example.com"
+print(url)
+resp = requests.get("http://www.example.com.br")
---
+++ exercises/0003-requests-sotu/c.py
@@ -0,0 +1,4 @@
+import requests
+url = "www.example.com"
+print(url)
+resp = requests.get("www.example.com.br")
